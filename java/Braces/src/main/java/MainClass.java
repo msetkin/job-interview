1dc1e94779344daffa2863a54f41d16eb6318f18
msetkin <63317990+msetkin@users.noreply.github.com>
2023-04-04 15:51:38 +0200

    Update MainClass.java

--- java/Braces/src/main/java/MainClass.java
+++ java/Braces/src/main/java/MainClass.java
@@ -1,8 +1,3 @@
-import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.Hashtable;
-import java.util.List;
 import org.junit.Assert;
 
 public class MainClass {
