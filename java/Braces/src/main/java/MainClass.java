6522402d86d60ae03dc43f1cc0781d96efda84f5
msetkin <63317990+msetkin@users.noreply.github.com>
2023-04-04 13:12:07 +0200

    Create MainClass.java

---
+++ java/Braces/src/main/java/MainClass.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.List;
+import org.junit.Assert;
+
+public class MainClass {
+
+  /**
+    * checks if the input string brace-wise balanced. "Balanced" means, that the number and the
+    * order of opening and closing brackets are symmetrical. See test-cases for more details.
+    *
+    * @param  someString  input string, can contain opening or closing braces: parentheses, square and curly brackets
+    * @return  true is the input string is "balanced" and false otherwise
+    */
+  public static boolean isStringBraceBalanced(String someString) {
+      /* your solution goes here */
+      return true;
+  }
+
+  public static void main(String[] args) {
+    Assert.assertTrue(isStringBraceBalanced("({[]})"));
+    Assert.assertFalse(isStringBraceBalanced("({[}])"));
+    Assert.assertFalse(isStringBraceBalanced("({[}]))))"));
+    Assert.assertTrue(isStringBraceBalanced(""));
+    Assert.assertFalse(isStringBraceBalanced("}{][)("));
+    Assert.assertFalse(isStringBraceBalanced("}{][)]("));
+    Assert.assertFalse(isStringBraceBalanced("(((({{{{{"));
+    Assert.assertFalse(isStringBraceBalanced("]]])}}})}}}}"));
+    Assert.assertTrue(isStringBraceBalanced("(()){}"));
+  }
+}
