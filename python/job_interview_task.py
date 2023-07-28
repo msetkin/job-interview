a81539a48dd497113af4d4c1b3b907fb699b839b
Mikhail Setkin <msetkin@gmail.com>
2023-07-28 21:07:50 +0200

    resolved the merge conflict

--- python/job_interview_task.py
+++ python/job_interview_task.py
@@ -1,8 +1,8 @@
-def is_braces_balanced(ent_string: str) -> bool:
+def is_braces_balanced(input_string: str) -> bool:
     """
         is_braces_balanced checks if the input string brace-wise balanced. "Balanced" means, that the number and the
         order of opening and closing brackets are symmetrical. See test-cases for more details.
-        :param some_string: input string, can contain opening or closing braces: parentheses, square and curly brackets
+        :param input_string: input string, can contain opening or closing braces: parentheses, square and curly brackets.
         :return: true is the input string is "balanced" and false otherwise
     """
     # your solution goes here
